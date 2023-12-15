fe8051ac8f811ed96c13effc2c086a9bd8649680
ValentinaBovkun <v.s.bovkun@student.khai.edu>
2023-12-15 14:07:31 +0200

    lab 4 #5

--- src/main/java/org/example/MainTest4.java
+++ src/main/java/org/example/MainTest4.java
@@ -1,5 +1,9 @@
 package org.example;
 import java.time.LocalDateTime;
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
 public class MainTest4 {
     public static void main(String[] args) {
         Mage mage = new Sorcerer();
@@ -13,5 +17,18 @@ public class MainTest4 {
         // получение даты и времени создания объекта Mage и вывод на экран
         LocalDateTime creationDateTime = mage.getCreationDate();
         System.out.println("Дата и время создания мага: " + creationDateTime);
+
+        /* запись объекта в файл */
+        writeToObjectsFile("objects.txt", mage.toString() + "\n" + creationDateTime);
     }
+    /* сама запись данных в файл */
+    public static void writeToObjectsFile(String fileName, String data) {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(fileName))) {
+            writer.write(data);
+            System.out.println("Объекты записаны в файл: " + fileName);
+        } catch (IOException e) {
+            System.err.println("Ошибка при записи в файл: " + e.getMessage());
+        }
+    }
+
 }
